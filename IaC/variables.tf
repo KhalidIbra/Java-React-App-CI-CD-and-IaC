13325767a3eb5cf29b3924996905c70cb853d5b4
KhalidIbra <khalidrk@hotmail.co.uk>
2024-11-16 21:07:31 +0000

    edited variables

--- IaC/variables.tf
+++ IaC/variables.tf
@@ -1,9 +1,3 @@
- variable "GCP_CREDENTIALS_FILE" {
-     type = string
-     default = "/Users/khalidibrahim/spring-boot-react-example/IaC /my-new-project-441421-8f2d27d1390b.json"
-     sensitive = true 
-}
-
 variable "GCP_PROJECT_ID" {
     type = string
     default = "my-new-project-441421"
